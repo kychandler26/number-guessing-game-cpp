7a56fa0b7490207d8e59f86047a24e339d7ffc64
Kyle Chandler <kychandler26@gmail.com>
2024-12-19 16:34:48 +1100

    I'm learning how to use functions in C++. I commented out the guess logic because I kept getting expression preceding parentheses of apparent call must have pointer-to function type as an error

--- start.cpp
+++ start.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello World!";
-    return 0;
+void makeGuess() {
+    cout << "This is where the guessing logic will go!";
+    // cout << "Pick a number between 1 and 100"; // Prompt the user
+    // cin >> user_guess; // Get user's guess from the keyboard
+    // testGuess(user_guess, target_numm);
 }
+
+void testGuess() {
+    cout << "This is where the testing logic will go!";
+    // Check whether the prompt is correct
+    // if (user_guess = target_num) {
+    //     cout << "You got the number good job!";
+    // } else if (user_guess > target_num) {
+    //     cout << "Higher!";
+    //     makeGuess();
+    // } else {
+    //     cout << "Lower!";
+    //     makeGuess();
+    // };
+}
+
+int main() 
+{
+    // Initialise the variables
+    int target_num = rand() % 100; // Initialise the target integer variable
+    int user_guess; // Initialise the user_guess variable
+    makeGuess();
+    testGuess();
+
+}
+
+
+
