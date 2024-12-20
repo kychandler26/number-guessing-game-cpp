201cccfbddf1b9ef4582dcf25b35936ca1288cbd
Kyle Chandler <kychandler26@gmail.com>
2024-12-20 13:35:38 +1100

    Fixed a silly logic mistake in the if statement that was causing the program to return higher when the number was lower and vice-versa. Also removed the seeding function from the random number function.

--- start.cpp
+++ start.cpp
@@ -1,35 +1,44 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
-void makeGuess() {
-    cout << "This is where the guessing logic will go!";
-    // cout << "Pick a number between 1 and 100"; // Prompt the user
-    // cin >> user_guess; // Get user's guess from the keyboard
-    // testGuess(user_guess, target_numm);
+// Forward declaration so testGuess function can use makeGuess before its defined
+void makeGuess(int target_num);
+int numberGenerator();
+
+void testGuess(int user_guess, int target_num) {
+    // Check whether the guess is correct
+    if (user_guess == target_num) {
+        cout << "You got the number good job!" <<endl;
+    } else if (user_guess > target_num) {
+        cout << "Lower! " << endl;
+        makeGuess(target_num);
+    } else {
+        cout << "Higher! " << endl;
+        makeGuess(target_num);
+    };
 }
 
-void testGuess() {
-    cout << "This is where the testing logic will go!";
-    // Check whether the prompt is correct
-    // if (user_guess = target_num) {
-    //     cout << "You got the number good job!";
-    // } else if (user_guess > target_num) {
-    //     cout << "Higher!";
-    //     makeGuess();
-    // } else {
-    //     cout << "Lower!";
-    //     makeGuess();
-    // };
+void makeGuess(int target_num) {
+    int user_guess;
+    cin >> user_guess; // Get user's guess from the keyboard
+
+    testGuess(user_guess, target_num);
+}
+
+int numberGenerator() {
+    
+    int target_num = (1+ (rand() % 100)); // Calculate the random number
+    return target_num;
+
 }
 
 int main() 
 {
-    // Initialise the variables
-    int target_num = rand() % 100; // Initialise the target integer variable
-    int user_guess; // Initialise the user_guess variable
-    makeGuess();
-    testGuess();
-
+    srand(time(0)); // Seed the random number function with current time
+    int target_num = numberGenerator();
+    cout << "Pick a number between 1 and 100: "; // Prompt the user
+    makeGuess(target_num); // Call the make guess function
 }
 
 
